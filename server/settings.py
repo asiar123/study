1755f1bcb1aef4b428a5195d12638a77cae5c2e4
Michael Steban <edgar-bucheli@hotmail.com>
2023-06-13 00:58:11 -0500

    Arreglo static

--- server/settings.py
+++ server/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'server.urls'
@@ -77,12 +78,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'railway',
-        'USER': 'root',
-        'PASSWORD': 'CiIWanKzAF4kgZiPBZs0',
-        'HOST': 'containers-us-west-98.railway.app',
-        'PORT': 6152
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db12',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
@@ -131,6 +132,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
